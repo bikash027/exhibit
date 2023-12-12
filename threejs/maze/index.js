18a822312b934cd49d9ed588779529d728b5885c
Bikash Singha <bikashsingha940@gmail.com>
2023-12-12 11:00:32 +0530

    maze: added textures

--- threejs/maze/index.js
+++ threejs/maze/index.js
@@ -1,77 +1,24 @@
 import * as THREE from 'three';
 import { PointerLockControls } from 'three/addons/controls/PointerLockControls.js';
-// import px from './resources/images/cubemaps/skyboxsun/px.jpg';
-// import nx from './resources/images/cubemaps/skyboxsun/nx.jpg';
-// import py from './resources/images/cubemaps/skyboxsun/py.jpg';
-// import ny from './resources/images/cubemaps/skyboxsun/ny.jpg';
-// import pz from './resources/images/cubemaps/skyboxsun/pz.jpg';
-// import nz from './resources/images/cubemaps/skyboxsun/nz.jpg';
 import { maze, mazeHash } from './maze.js';
-// console.log(Object.keys(px));
 
 const canvas = document.querySelector('#canvas');
 const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-const fov = 75;
+const fov = 45;
 const aspect = window.innerWidth / window.innerHeight;  // the canvas default
 const near = 0.1;
-const far = 1000;
+const far = 100;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 camera.position.set(0, 0.3, -0.75);
 camera.lookAt(new THREE.Vector3(1,0.1,0));
-
 const scene = new THREE.Scene();
-
 const controls = new PointerLockControls(camera, document.body);
 
-
-// Create a plane geometry
-const planeGeometry = new THREE.PlaneGeometry(34,20);
-
-// Create a blue material
-const groundMaterial = new THREE.MeshPhongMaterial({
-    color: '#ada083', // #d7cbb1
-    side: THREE.DoubleSide
-});
-
-// Create a plane mesh with the geometry and material
-const planeMesh = new THREE.Mesh(planeGeometry, groundMaterial);
-// Rotate the grid by 90 degrees
-planeMesh.rotation.x = Math.PI / 2;
-planeMesh.position.set(16,0,9);
-
-scene.add(planeMesh);
-
-maze.forEach((row, rowNum) =>{
-    for(const block of row){
-        const bushGeometry = new THREE.BoxGeometry(block[1] - block[0], 0.6, 1);
-        const bushMaterial = new THREE.MeshPhongMaterial({
-            color: '#00ff00',
-            side: THREE.FrontSide
-        });
-        const bushMesh = new THREE.Mesh(bushGeometry, bushMaterial);
-        const centerX = (block[0] + block[1])/2;
-        const centerY = 0.3;
-        const centerZ = rowNum + 0.5;
-        bushMesh.position.set(centerX, centerY, centerZ);
-        scene.add(bushMesh);
-    }
-})
-
-
-const color = 0xFFFFFF;
-const intensity = 1;
-const light = new THREE.AmbientLight(color, intensity);
-scene.add(light);
-
-const sunLight = new THREE.DirectionalLight('#e5dfd5', 1);
-sunLight.position.set(-Math.sin(Math.PI/4), Math.sin(25*Math.PI/180), -Math.sin(Math.PI/4));
-scene.add(sunLight);
-
-
-const loader = new THREE.CubeTextureLoader();
-const texture = loader.load([
+const loadManager = new THREE.LoadingManager()
+const loader = new THREE.CubeTextureLoader(loadManager);
+const backgroundTexture = loader.load([
     './resources/images/cubemaps/skyboxsun/px.jpg',
     './resources/images/cubemaps/skyboxsun/nx.jpg',
     './resources/images/cubemaps/skyboxsun/py.jpg',
@@ -79,14 +26,127 @@ const texture = loader.load([
     './resources/images/cubemaps/skyboxsun/pz.jpg',
     './resources/images/cubemaps/skyboxsun/nz.jpg'
 ]);
-scene.background = texture;
+
+scene.background = backgroundTexture;
+
+const color = 0xFFFFFF;
+const intensity = 0.5;
+const light = new THREE.AmbientLight(color, intensity);
+scene.add(light);
 
 
+const sunLight = new THREE.DirectionalLight('#e5dfd5', 1);
+sunLight.position.set(-Math.sin(Math.PI/4), Math.sin(25*Math.PI/180), -Math.sin(Math.PI/4));
+scene.add(sunLight);
+
+// scene.fog = new THREE.Fog(0xc2b69e, near, 30);
+// const pmremGenerator = new THREE.PMREMGenerator(renderer);
+// const pmrem = pmremGenerator.fromScene(scene);
+
+
+
+
+const textureLoader = new THREE.TextureLoader(loadManager);
+// const bumpMapGround = textureLoader.load('./resources/images/textures/park_dirt_disp_1k.png');
+// const roughMapGround = textureLoader.load('./resources/images/textures/brown_mud_leaves_01_rough_1k.png');
+// bumpMapGround.wrapS = THREE.RepeatWrapping;
+// roughMapGround.wrapS = THREE.RepeatWrapping;
+// bumpMapGround.wrapT = THREE.RepeatWrapping;
+// roughMapGround.wrapT = THREE.RepeatWrapping;
+// bumpMapGround.repeat.set(60, 60);
+// roughMapGround.repeat.set(34, 20);
+// const groundMaterial = new THREE.MeshStandardMaterial({
+//     color: '#d7cbb1',//'#736c2c', //'#736a45', //'#ada083', // #d7cbb1
+//     side: THREE.FrontSide,
+//     // displacementMap: bumpMapGround,
+//     // roughnessMap: roughMapGround,
+//     roughness: 1,
+//     envMap: backgroundTexture
+// });
+
+
+const diffuseMap = textureLoader.load('./resources/images/textures/brick_wall_005_diff_1k.png');
+// const roughMap = textureLoader.load('./resources/images/textures/mossy_stone_wall_rough_1k.png');
+const normalMap = textureLoader.load('./resources/images/textures/brick_wall_005_nor_gl_1k.png');
+// const dispMap = textureLoader.load('./resources/images/textures/brick_wall_005_disp_1k.png');
+diffuseMap.wrapS = THREE.RepeatWrapping;
+// dispMap.wrapS = THREE.RepeatWrapping;
+// roughMap.wrapS = THREE.RepeatWrapping;
+normalMap.wrapS = THREE.RepeatWrapping;
+diffuseMap.center.set(0.5, 0.5);
+// dispMap.center.set(0.5, 0.5);
+// roughMap.center.set(0.5, 0.5);
+normalMap.center.set(0.5, 0.5);
+diffuseMap.rotation = Math.PI;
+// dispMap.rotation = Math.PI;
+// roughMap.rotation = Math.PI;
+normalMap.rotation = Math.PI;
 
 const playButton = document.getElementById('playButton');
-playButton.addEventListener('click', () => {
-    controls.lock();
-})
+loadManager.onLoad = () => {
+    // // Create a plane geometry
+    // const planeGeometry = new THREE.PlaneGeometry(60,60);
+    // // Create a plane mesh with the geometry and material
+    // const planeMesh = new THREE.Mesh(planeGeometry, groundMaterial);
+    // // Rotate the grid by 90 degrees
+    // planeMesh.rotation.x = Math.PI / 2;
+    // planeMesh.position.set(16,0,9);
+
+    // scene.add(planeMesh);
+    const baseWallMaterial = new THREE.MeshStandardMaterial({
+        map: diffuseMap,
+        normalMap: normalMap,
+        envMap: backgroundTexture,
+        roughness: 1
+    });
+    maze.forEach((row, rowNum) =>{
+        for(const block of row){
+            const bushGeometry = new THREE.BoxGeometry(block[1] - block[0], 0.6, 1);
+            const materials = [];
+            //px
+            materials.push(baseWallMaterial);
+            //nx
+            materials.push(baseWallMaterial);
+            //py
+            materials.push(null);
+            //ny
+            materials.push(null);
+            //pz
+            const diffuseMapClone = diffuseMap.clone();
+            diffuseMapClone.repeat.set(block[1] - block[0], 1);
+            // const roughMapClone = roughMap.clone();
+            // roughMapClone.repeat.set(block[1] - block[0], 1);
+            const normalMapClone = normalMap.clone();
+            normalMapClone.repeat.set(block[1] - block[0], 1);
+            // const dispMapClone = dispMap.clone();
+            // dispMapClone.repeat.set(block[1] - block[0], 1);
+            const clonedWallMaterial = new THREE.MeshStandardMaterial({
+                map: diffuseMapClone,
+                normalMap: normalMapClone,
+                envMap: backgroundTexture,
+                roughness: 1
+            })
+            materials.push(clonedWallMaterial);
+            //nz
+            materials.push(clonedWallMaterial);
+            diffuseMapClone.needsUpdate = true;
+            normalMapClone.needsUpdate = true;
+            // const bushMaterial = new THREE.MeshStandardMaterial({
+            //     map: diffuseMap,
+            //     roughnessMap: roughMap
+            // });
+            const bushMesh = new THREE.Mesh(bushGeometry, materials);
+            const centerX = (block[0] + block[1])/2;
+            const centerY = 0.3;
+            const centerZ = rowNum + 0.5;
+            bushMesh.position.set(centerX, centerY, centerZ);
+            scene.add(bushMesh);
+        }
+    })
+    playButton.addEventListener('click', () => {
+        controls.lock();
+    })
+}
 
 controls.addEventListener('lock', () => {
     playButton.parentElement.style.display = 'none';
@@ -172,7 +232,7 @@ function render(time) {
     time *= 0.001;  // convert time to seconds
    
     if (controls.isLocked) {
-        const delta = 0.03;
+        const delta = 0.02;
 
         if (moveForward) {
             controls.moveForward(delta);
